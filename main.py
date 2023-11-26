02d7d600ffbbbb61630b506cc9c469b76d9db35f
DImiir <d.i.mironenko@yandex.ru>
2023-11-26 16:32:34 +0300

    All done, but it is Cappuccino

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sqlite3
 class Espresso(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.Window = None
         uic.loadUi('main.ui', self)
         con = sqlite3.connect("coffee.sqlite")
         cur = con.cursor()
@@ -19,6 +20,56 @@ class Espresso(QMainWindow):
                 self.table.setItem(i, j, QTableWidgetItem(str(elem[j])))
         self.table.setHorizontalHeaderLabels(['ID', 'Название сорта', 'Степень обжарки', 'Молотый/в зернах',
                                               'Описание вкуса', 'Цена', 'Объём упаковки'])
+        self.add_change_window.clicked.connect(self.show_add_change_window)
+
+    def show_add_change_window(self):
+        self.close()
+        self.Window = AddChange()
+        self.Window.show()
+
+
+class AddChange(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.Window = None
+        self.add_btn.clicked.connect(self.add_func)
+        self.change_btn.clicked.connect(self.change_func)
+        self.back_btn.clicked.connect(self.back_func)
+
+    def add_func(self):
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+        count = cur.execute(f"""SELECT ID FROM infoffee""").fetchall()
+        if len(count) == 0:
+            id_number = 1
+        else:
+            id_number = count[-1][0] + 1
+        cur.execute(f"""INSERT INTO infoffee 
+                (ID, name, stepen, condition, taste, price, volume)
+                VALUES 
+                (?, ?, ?, ?, ?, ?, ?)""", (id_number, self.name_line.text(), self.stepen_line.text(),
+                                           self.condition_line.text(), self.taste_line.text(), self.price_line.text(),
+                                           self.volume_line.text()))
+        con.commit()
+        con.close()
+
+    def change_func(self):
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+        cur.execute(f"""UPDATE infoffee
+                        SET name = ?, stepen = ?, condition = ?, taste = ?, price = ?, volume = ?
+                        WHERE ID = ?""", (self.name_change.text(),
+                                          self.stepen_change.text(), self.condition_change.text(),
+                                          self.taste_change.text(), self.price_change.text(),
+                                          self.volume_change.text(), int(self.which_change.text())))
+        con.commit()
+        con.close()
+
+    def back_func(self):
+        self.close()
+        self.Window = Espresso()
+        self.Window.show()
 
 
 if __name__ == '__main__':
